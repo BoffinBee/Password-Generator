9effd34abbc907350eec2562412ceb0c34de1796
BoffinBee <boffinbee@gmail.com>
2022-08-01 13:30:13 +0100

    Added a copy button the password area with no funtionality yet, implemented  hover and click effects to the buttons.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const pass1El = document.getElementById("pass-1");
 const pass2El = document.getElementById("pass-2");
 
+const copyEl1 = document.getElementById("copy-btn1");
+const copyEl2 = document.getElementById("copy-btn2");
+copyEl1.style.visibility = "hidden";
+copyEl2.style.visibility = "hidden";
+
+
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z", "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?", "/", "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 
@@ -24,6 +30,10 @@ function genKeys () {
 
 
 function genPass() {
-    pass1El.textContent = genKeys();
-    pass2El.textContent = genKeys();
+    const pass1 = genKeys();
+    const pass2 = genKeys();
+    pass1El.textContent = pass1;
+    pass2El.textContent = pass2;
+    copyEl1.style.visibility = "visible";
+    copyEl2.style.visibility = "visible";
 }
